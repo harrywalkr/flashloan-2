336016421ef2ccb603dccfd8e5606a34b5ab0cea
harrywalkr <116752427+harrywalkr@users.noreply.github.com>
2023-05-08 08:07:30 +0400

    Initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     goerli: {
       url: "https://sepolia.infura.io/v3/2d70b12bcadb4ad1a354dc70aebf0211",
-      accounts: ["d2364317cf4a858f3bb6e7ce89e6b4041780d39fab8e7b60cdb7a754819081cc"],
+      accounts: [""],
     },
   },
 };
